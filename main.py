437edf45924e4995b98c7237ff0c196ca1c54f30
dbachali <dbachali@stevens.edu>
2021-05-09 13:34:11 -0400

    aad test flask api

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello_world():
+    return "Hellow World"
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port=8000)
+
