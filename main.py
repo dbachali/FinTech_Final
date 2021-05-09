f7f3c2b9b73a02b595e90711e025f128492e9f0e
dbachali <dbachali@stevens.edu>
2021-05-09 14:08:06 -0400

    add a route

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hellow World"
+    return "Hello World"
 
+@app.route('/hello/<name>', methods=['GET', 'POST'])
+def hello_world(name):
+    return "Hello, \|.".format(name)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port=8000)
